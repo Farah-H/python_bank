dcefd9ab2a67bf076fc7ac401518b2b88c60f0f8
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-09 07:56:12 +0000

    Update account_holder_class.py

--- account_holder_class.py
+++ account_holder_class.py
@@ -29,22 +29,3 @@ class AccountHolderDetails:
     def account_holder(self):
         del self.__name, self.__address, self.__age
     
-# Inherit Account holder class into MyAccount
-
-
-# Create a class called MyAccount which represents a bank account, 
-# having as attributes: accountNumber (numeric type), balance.
-
-# Create a constructor () method? with parameters: accountNumber, balance.
-
-# Create a Deposit() method which manages the deposit actions.
-# Create a Withdrawal() method which manages withdrawals actions.
-# Create an bankFees() method to apply the bank fees with a percentage of 5% of the balance account.
-# Create a display() method to display account details.
-
-# Create manageAccount class and import everything from BankAccount class
-# call all methods in manageAccount class that have are available from parent class
-
-# Create a display() method to display account details.
-
-# Create a README.md for your docu
